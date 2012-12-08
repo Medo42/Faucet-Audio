e63527f2e236bb74c9bccfae956277f9eecc828b
nagn <mlin4589@gmail.com>
2012-12-08 10:59:14 -0500

    Updated Faucet Audio for panning/volume control Unfortunately, we might not be able to take advantage of the sfx commands, as there is no reasonable way of distinguishing between different sfx handles, but luckily that's what audio_play is for

--- faucet/dllapi.cpp
+++ faucet/dllapi.cpp
@@ -1,35 +1,195 @@
 #include <cstring>
 #include <audiere.h>
 #include <algorithm>
-
+#include <windows.h>
+#include <sstream>
+#include <string>
 #include "HandleMap.hpp"
 
 #define DLLEXPORT extern "C" __declspec(dllexport)
 
 audiere::AudioDevicePtr device;
 HandleMap<audiere::OutputStream> outputStreamPointers;
-
-DLLEXPORT double audio_play(const char *filename, double volume, double repeat) {
+HandleMap<audiere::SoundEffect>  soundEffectPointers;
+bool debugMode = false;
+void MsgBox(const char* caption,const char* message) {
+        MessageBox(0, message, caption, MB_OK);
+}
+DLLEXPORT double fa_set_debugmode(double flag) {
+    if (flag)
+        debugMode = true;
+    else
+        debugMode = false;
+    return 0;
+}
+DLLEXPORT double audio_create(const char *filename, double stream) {
 	if(!device) return -1;
-	volume = std::min(1.0, std::max(0.0, volume));
-	audiere::OutputStreamPtr sound(audiere::OpenSound(device, filename, true));
-	if (!sound) {
+	audiere::OutputStreamPtr audio(audiere::OpenSound(device, filename, stream));
+	if (!audio) {
 		return -1;
 	} else {
-		sound->setRepeat(repeat);
-		sound->setVolume((float)volume);
-		sound->play();
-		return outputStreamPointers.allocate(sound);
+		return outputStreamPointers.allocate(audio);
 	}
 }
-
+DLLEXPORT double audio_play(double handle) {
+	audiere::OutputStreamPtr audio = outputStreamPointers.find(handle);
+	if (!audio) {
+		return -1;
+	} else {
+		audio->play();
+		return 0;
+	}
+}
+DLLEXPORT double audio_set_repeat(double handle, double repeat) {
+	audiere::OutputStreamPtr audio = outputStreamPointers.find(handle);
+	if (!audio) {
+		return -1;
+	} else {
+	    audio->setRepeat(repeat);
+		return 0;
+	}
+}
+DLLEXPORT double audio_set_volume(double handle, double volume) {
+	volume = std::min(1.0, std::max(0.0, volume));
+	audiere::OutputStreamPtr audio = outputStreamPointers.find(handle);
+	if (!audio) {
+		return -1;
+	} else {
+	    audio->setVolume((float)volume);
+		return 0;
+	}
+}
+DLLEXPORT double audio_set_pan(double handle, double pan) {
+	pan = std::min(1.0, std::max(-1.0, pan));
+	audiere::OutputStreamPtr audio = outputStreamPointers.find(handle);
+	if (!audio) {
+		return -1;
+	} else {
+	    audio->setPan((float)pan);
+		return 0;
+	}
+}
+DLLEXPORT double audio_get_volume(double handle) {
+	audiere::OutputStreamPtr audio = outputStreamPointers.find(handle);
+	if (!audio) {
+		return -1;
+	} else {
+	    return audio->getVolume();
+	}
+}
+DLLEXPORT double audio_get_pan(double handle) {
+	audiere::OutputStreamPtr audio = outputStreamPointers.find(handle);
+	if (audio) {
+		return audio->getPan();
+	} else {
+        if (debugMode) {
+            std::ostringstream sstream;
+            sstream << "Requested Pan from nonexistiant Handle! "<< std::endl << "Handle: " << handle << std::endl << "Returning 0.";
+            std::string message = sstream.str();
+            const char * cmessage = message.c_str();
+            MsgBox("Error", cmessage);
+        }
+        return 0;
+    }
+}
 DLLEXPORT double audio_stop(double handle) {
-	audiere::OutputStreamPtr sound = outputStreamPointers.find(handle);
-	if(sound) {
-		sound->stop();
+	audiere::OutputStreamPtr audio = outputStreamPointers.find(handle);
+	if(audio) {
+		audio->stop();
+		return 0;
+	}
+	return -1;
+}
+DLLEXPORT double audio_release(double handle) {
+	audiere::OutputStreamPtr audio = outputStreamPointers.find(handle);
+	if(audio) {
+		audio->stop();
 		outputStreamPointers.release(handle);
 	}
 	return 0;
+}
+DLLEXPORT double audio_is_playing(double handle) {
+    audiere::OutputStreamPtr audio = outputStreamPointers.find(handle);
+    if (!audio) return -1;
+    if (audio -> isPlaying()) {
+        return 1;
+    }
+    return 0;
+}
+DLLEXPORT double sfx_create(const char *filename) {
+	if(!device) return -1;
+    audiere::SoundEffectPtr sfx(audiere::OpenSoundEffect(device, filename, audiere::SoundEffectType::SINGLE));
+	if (!sfx) {
+		return -1;
+	} else {
+		return soundEffectPointers.allocate(sfx);
+	}
+}
+DLLEXPORT double sfx_set_pan(double handle, double pan)  {
+    audiere::SoundEffectPtr sfx = soundEffectPointers.find(handle);
+	pan = std::min(1.0, std::max(-1.0, pan));
+    if (sfx) {
+        sfx->setPan((float)pan);
+        return(0);
+    }
+	return -1;
+}
+DLLEXPORT double sfx_get_pan(double handle)  {
+    audiere::SoundEffectPtr sfx = soundEffectPointers.find(handle);
+    if (sfx) {
+        return sfx->getPan();
+    }else{
+        if (debugMode) {
+            std::ostringstream sstream;
+            sstream << "Requested Pan from nonexistiant Handle! "<< std::endl << "Handle: " << handle << std::endl << "Returning 0.";
+            std::string message = sstream.str();
+            const char * cmessage = message.c_str();
+            MsgBox("Error", cmessage);
+        }
+        return 0;
+    }
+}
+DLLEXPORT double sfx_set_volume(double handle, double volume)  {
+    audiere::SoundEffectPtr sfx = soundEffectPointers.find(handle);
+	volume = std::min(1.0, std::max(0.0, volume));
+    if (sfx) {
+        sfx->setVolume((float)volume);
+        return 0;
+    }
+    return -1;
+}
+DLLEXPORT double sfx_get_volume(double handle)  {
+    audiere::SoundEffectPtr sfx = soundEffectPointers.find(handle);
+    if (sfx) {
+        return sfx->getVolume();
+    }else{
+        return -1;
+    }
+}
+DLLEXPORT double sfx_release(double handle)  {
+    audiere::SoundEffectPtr sfx = soundEffectPointers.find(handle);
+    if(sfx) {
+		sfx->stop();
+		soundEffectPointers.release(handle);
+		return 0;
+	}
+	return -1;
+}
+DLLEXPORT double sfx_play(double handle) {
+    audiere::SoundEffectPtr sfx = soundEffectPointers.find(handle);
+    if (sfx) {
+        sfx->play();
+        return 0;
+    }
+    return -1;
+}
+DLLEXPORT double sfx_stop(double handle) {
+    audiere::SoundEffectPtr sfx = soundEffectPointers.find(handle);
+    if (sfx) {
+        sfx->stop();
+        return 0;
+    }
+    return -1;
 }
 
 DLLEXPORT double dllStartup() {
@@ -39,6 +199,7 @@ DLLEXPORT double dllStartup() {
 
 DLLEXPORT double dllShutdown() {
 	outputStreamPointers.releaseAll();
+	soundEffectPointers.releaseAll();
 	device = NULL;
 	return 0;
 }
