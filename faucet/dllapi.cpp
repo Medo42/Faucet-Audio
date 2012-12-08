c5178d0973d2e94dca306c53001704904ef6856b
nagn <mlin4589@gmail.com>
2012-12-08 12:08:36 -0500

    Added some basic debugging functionality to the codeblocks debug project

--- faucet/dllapi.cpp
+++ faucet/dllapi.cpp
@@ -1,27 +1,12 @@
 #include <cstring>
 #include <audiere.h>
 #include <algorithm>
-#include <windows.h>
-#include <sstream>
-#include <string>
 #include "HandleMap.hpp"
-
 #define DLLEXPORT extern "C" __declspec(dllexport)
 
 audiere::AudioDevicePtr device;
 HandleMap<audiere::OutputStream> outputStreamPointers;
 HandleMap<audiere::SoundEffect>  soundEffectPointers;
-bool debugMode = false;
-void MsgBox(const char* caption,const char* message) {
-        MessageBox(0, message, caption, MB_OK);
-}
-DLLEXPORT double fa_set_debugmode(double flag) {
-    if (flag)
-        debugMode = true;
-    else
-        debugMode = false;
-    return 0;
-}
 DLLEXPORT double audio_create(const char *filename, double stream) {
 	if(!device) return -1;
 	audiere::OutputStreamPtr audio(audiere::OpenSound(device, filename, stream));
@@ -82,23 +67,20 @@ DLLEXPORT double audio_get_pan(double handle) {
 	if (audio) {
 		return audio->getPan();
 	} else {
-        if (debugMode) {
-            std::ostringstream sstream;
-            sstream << "Requested Pan from nonexistiant Handle! "<< std::endl << "Handle: " << handle << std::endl << "Returning 0.";
-            std::string message = sstream.str();
-            const char * cmessage = message.c_str();
-            MsgBox("Error", cmessage);
-        }
-        return 0;
+	    #ifdef DEBUG
+        std::cout << "Requested Pan from nonexistiant Handle! "<< std::endl << "Handle: " << handle << std::endl << "Returning 0.";
+        #endif
+        return (-1);
     }
 }
+
 DLLEXPORT double audio_stop(double handle) {
 	audiere::OutputStreamPtr audio = outputStreamPointers.find(handle);
 	if(audio) {
 		audio->stop();
 		return 0;
 	}
-	return -1;
+	return (-1);
 }
 DLLEXPORT double audio_release(double handle) {
 	audiere::OutputStreamPtr audio = outputStreamPointers.find(handle);
@@ -139,16 +121,13 @@ DLLEXPORT double sfx_get_pan(double handle)  {
     if (sfx) {
         return sfx->getPan();
     }else{
-        if (debugMode) {
-            std::ostringstream sstream;
-            sstream << "Requested Pan from nonexistiant Handle! "<< std::endl << "Handle: " << handle << std::endl << "Returning 0.";
-            std::string message = sstream.str();
-            const char * cmessage = message.c_str();
-            MsgBox("Error", cmessage);
+        #ifdef DEBUG
+        std::cout << "Requested Pan from nonexistiant Handle! "<< std::endl << "Handle: " << handle << std::endl << "Returning 0.";
+        #endif
+        return (-1);
         }
-        return 0;
+
     }
-}
 DLLEXPORT double sfx_set_volume(double handle, double volume)  {
     audiere::SoundEffectPtr sfx = soundEffectPointers.find(handle);
 	volume = std::min(1.0, std::max(0.0, volume));
@@ -203,3 +182,21 @@ DLLEXPORT double dllShutdown() {
 	device = NULL;
 	return 0;
 }
+#ifdef DEBUG
+int main(void){
+    dllStartup();
+    double sound1, sound2;
+
+    sound1 = audio_create("test/menumusic1.ogg", true);
+    sound2 = audio_create("test/menumusic2.ogg", true);
+    audio_set_pan(sound1, -1);
+    audio_set_pan(sound2, 1);
+    std::cout << sound1 << std::endl << sound1 << std::endl;
+    audio_play(sound1);
+    audio_play(sound2);
+    std::cout << "PRESS ENTER TO CONTINUE";
+    std::cin.ignore();
+    dllShutdown();
+    return (0);
+}
+#endif
